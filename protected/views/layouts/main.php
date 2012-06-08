3a2ae558815c9ef8b8968d8d5722cf9de262d2cc
chuck911 <chuck911@qq.com>
2012-06-08 18:24:42 +0800

    add commit bar

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -7,14 +7,6 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<!-- Le styles -->
-	<style>
-	  body {
-		padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
-	  }
-	</style>
-
-
 	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -63,6 +55,6 @@
 	<div class="container">
 	<?php echo $content ?>
 	</div> <!-- /container -->
-
+	<a id="feedback" class="btn btn-primary" href="http://ideaworks.cn/v/index.php?p=/categories/matou-it">开发建议</a>
   </body>
 </html>
