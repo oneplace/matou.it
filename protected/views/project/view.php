e5b1d36d792bc264631517ee0d405e265203d289
chuck911 <chuck911@qq.com>
2012-06-09 18:06:18 +0800

    update project view

--- protected/views/project/view.php
+++ protected/views/project/view.php
@@ -10,20 +10,26 @@
             <dl class="clearfix">
               <dt>官方网站：</dt>
               <dd><?php echo CHtml::link($project->url,$project->url) ?></dd>
-              <dt>作者：</dt>
-              <dd><?php echo $project->author ?></dd>
+							<?php if ($project->author): ?>
+							<dt>作者：</dt>
+	            <dd><?php echo $project->author ?></dd>
+							<?php endif ?>
             </dl>
             <h6>标签：</h6>
             <ul id="tag-list">
 							<?php foreach ($project->tags as $tag): ?>
-							<li><?php echo CHtml::link($tag->name,'#',array('class'=>'label label-info')) ?></li>	
+							<li><?php echo CHtml::link($tag->name,array('project/tagged','tag'=>$tag->name),array('class'=>'label label-info')) ?></li>	
 							<?php endforeach ?>
             </ul>
           </div>
           <div class="span2">
 						<?php echo CHtml::link('更新信息',array('project/update','id'=>$project->id),array('class'=>'btn span1 btn-primary')) ?>
+						<?php if ($project->repo): ?>
             <a href="<?php echo $project->repo ?>" class="btn span1 btn-info">Github</a>
+						<?php endif ?>
+						<?php if ($project->demo): ?>
             <a href="<?php echo $project->demo ?>" class="btn span1 btn-info">Demo</a>
+						<?php endif ?>
           </div>
         </div>
       </div>
@@ -37,10 +43,6 @@
       <div class="span4">
         <h3>相关文章：</h3>
         <ul id="article-list" class="clearfix">
-          <li><a href="#">Designed for everyone, everywhere.</a></li>
-          <li><a href="#">Designed for everyone, everywhere.</a></li>
-          <li><a href="#">Designed for everyone, everywhere.</a></li>
-          <li><a href="#">Designed for everyone, everywhere.</a></li>
           <li><a href="#">Designed for everyone, everywhere.</a></li>
         </ul>
       </div>
