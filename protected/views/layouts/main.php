3b8f9d35032a7e7958585db485dcc053cfb81376
chuck911 <chuck911@qq.com>
2012-06-20 12:03:24 +0800

    add seo meta

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="zh">
   <head>
-	<meta charset="utf-8">
-	<title>码头 - 开源代码的集散地</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<?php Yii::app()->controller->widget('ext.seo.widgets.SeoHead',array(
+	    'httpEquivs'=>array(
+	        'Content-Type'=>'text/html; charset=utf-8',
+	        'Content-Language'=>'zh-CN'
+	    ),
+	    'defaultDescription'=>'码头，开源代码的集散地。程序员的充电房',
+	    'defaultKeywords'=>'开源，代码，分享，码头，创业',
+	)); ?>
 
 	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
