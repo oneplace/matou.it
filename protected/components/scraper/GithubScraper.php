341284103437878eae5f4d33f7b45327108492a8
chuck911 <chuck911@qq.com>
2012-06-21 12:53:31 +0800

    normalize github url

--- protected/components/scraper/GithubScraper.php
+++ protected/components/scraper/GithubScraper.php
@@ -4,6 +4,7 @@ class GithubScraper extends Scraper
 	public function scrap()
 	{
 		$path = parse_url($this->url,PHP_URL_PATH);
+		$path = rtrim($path,'/');
 		$infoJson = file_get_contents('https://api.github.com/repos'.$path);
 		$info = json_decode($infoJson,true);
 		$readmeJson = file_get_contents('https://api.github.com/repos'.$path.'/readme');
